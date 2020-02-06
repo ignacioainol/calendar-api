4d0edef5f306c5ec0f278c91a7ddd10308811e71
Ignacio <shoik.xtazy@gmail.com>
2020-02-05 21:55:12 -0300

    add user database

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,7 @@ const save = async (user) => {
                   VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
                   RETURNING *`;
     
-    const values = [user.rut, user.names, user.lastnames, user.entity_id, user.commune_id, user.login, user.password, ];
+    const values = [user.rut, user.names, user.lastnames, user.entity_id, user.commune_id, user.login, user.password, user.phone, user.email, user.profile_id];
     const result = await connection.query(query, values);
     connection.release();
     return result.rows[0];
--- src/models/connect.js
+++ src/models/connect.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
+
 const { Pool } = require("pg");
 
+
 const pool = new Pool({
-    user : "krgwtsdd",
-    host : "rajje.db.elephantsql.com",
-    database : "krgwtsdd",
-    password : "u9oI8vV2OJMdiA1ECpYdVKe3QClRXr0Y"
+    user : process.env.USER_DATABASE,
+    host : process.env.HOST_DATABASE,
+    database : process.env.DATABASE_NAME,
+    password : process.env.PASSWORD_DATABASE
 });
+
 
 const connecting = async () => {
     const client = await pool.connect();
